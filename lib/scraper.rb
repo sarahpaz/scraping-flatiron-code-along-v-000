e3cb4137f9635298ee726e60c8585d77a5b72f21
Sarah Paz <sarah.paz@gmail.com>
2019-02-06 04:20:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
   def get_page
     
   end
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -20,8 +20,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
